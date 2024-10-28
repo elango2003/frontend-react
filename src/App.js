f0104f37a6fc152c2c6969ec75b07b76c6918af5
Elangovan <116818847+elango2003@users.noreply.github.com>
2024-10-28 13:11:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";  // Import HashRouter
 import Adminlogin from "./components/login/Adminlogin";
 import Home from "./components/Dashboard/Home/Home";
 import Dashboard from "./components/Dashboard/Home/Dashboard";
@@ -10,21 +10,22 @@ import { ToastContainer } from "react-toastify";
 import Treatment from "./components/Home/Treatment";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import History from "./components/History/History";
+
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>  {/* Replace BrowserRouter with HashRouter */}
         <Routes>
           <Route exact path="/" element={<Adminlogin />} />
           <Route exact path="/dashboard" element={<Dashboard />} />
-          <Route path="/dashboard/create" element={<Create />}></Route>
-          <Route path='/dashboard/details/:userid' element={<Detail />}></Route>
-          <Route path='/dashboard/edit/:userid' element={<Edit />}></Route>
-          <Route path='/dashboard/treatment/:userid' element={<Treatment />}></Route>
-          <Route path='/dashboard/medication' element={<Treatment />}></Route>
-          <Route path='/dashboard/history/:userid' element={<History />}></Route>
+          <Route path="/dashboard/create" element={<Create />} />
+          <Route path="/dashboard/details/:userid" element={<Detail />} />
+          <Route path="/dashboard/edit/:userid" element={<Edit />} />
+          <Route path="/dashboard/treatment/:userid" element={<Treatment />} />
+          <Route path="/dashboard/medication" element={<Treatment />} />
+          <Route path="/dashboard/history/:userid" element={<History />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
       <ToastContainer />
     </div>
   );
